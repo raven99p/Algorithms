bdfdf6b9b15ab72a4281eae18ff43db61787d3c1
George Al-Baboul <58749624+sphantom99@users.noreply.github.com>
2021-04-15 22:15:58 +0300

    Add files via upload

--- ledatest.cpp
+++ ledatest.cpp
@@ -3,50 +3,26 @@
 #include "LEDA/core/queue.h"
 using namespace leda;
 
-int main()
+list<node> my_BFS(const graph &G, node s, node_array<int> &dist, node_array<edge> &pred)
 {
-
-    graph G;
-    node center = G.new_node();
     queue<node> Q;
-    int x;
-    /*
-    for (int i = 0; i < 10; i++)
-    {
-        node v = G.new_node();
-        G.new_edge(center, v);
-    }*/
-    node one = G.new_node();
-    node two = G.new_node();
-    node three = G.new_node();
-    node four = G.new_node();
-    node five = G.new_node();
-    G.new_edge(one, two);
-    G.new_edge(one, three);
-    G.new_edge(two, four);
-    G.new_edge(three, five);
-    G.new_edge(center, one);
-    edge e;
-    /*
-    bool visited = new bool[10];
-    for (int i = 0; i < 10; i++)
-    {
-        visited[i] = false;
-    }*/
-    list<int> Colors;
-
-    std::cout << Colors;
-
-    Q.append(center);
+    node_array<int> Colors(G, 0);
+    node_array<bool> visited(G, false);
+    //node_array<edge> pred(G, NULL);
+    int levelCounter = 0;
+    //node_array<int> dist(G, 0);
+    Q.append(s);
     Q.append(NULL);
 
-    //Colors[0] = 1;
     int color = 2;
-    int counter = 1;
+    edge e;
+    visited[s] = true;
+    pred[s] = NULL;
+    node current_node;
     while (!Q.empty())
     {
 
-        node current_node = Q.pop();
+        current_node = Q.pop();
 
         if (current_node == NULL)
         {
@@ -59,6 +35,7 @@ int main()
                 color = 2;
             }
             std::cout << "different level \n";
+            levelCounter++;
             Q.append(NULL);
             if (Q.top() == NULL)
             {
@@ -71,20 +48,74 @@ int main()
         }
         else
         {
-            Colors.push_back(color);
-            list<edge> outgoing_edges = G.out_edges(current_node);
+            Colors[current_node] = color;
+            //visited[current_node] = true;
+            dist[current_node] = levelCounter;
             forall_out_edges(e, current_node)
             {
-                Q.append(G.target(e));
-                G.print_edge(e);
-                std::cout << "\n";
+                if (visited[G.target(e)] == false)
+                {
+                    Q.append(G.target(e));
+                    visited[G.target(e)] = true;
+                    pred[G.target(e)] = e;
+                    G.print_edge(e);
+                    std::cout << "\n";
+                }
             }
         }
-
-        std::cout << "Passed the for loop \n";
-        counter++;
     }
-    std::cout << Colors;
-    std::cout << "This is your conciousness \nYou fucked up\n";
+    node v;
+    forall_nodes(v, G)
+    {
+        std::cout << Colors[v];
+    }
+    std::cout << "\n";
+    forall_nodes(v, G)
+    {
+        std::cout << visited[v];
+    }
+    std::cout << "\n";
+    forall_nodes(v, G)
+    {
+        std::cout << dist[v];
+    }
+    std::cout << "\n";
+    list<node> visitedList;
+    forall_nodes(v, G)
+    {
+        if (visited[v] == true)
+        {
+            visitedList.append(v);
+        }
+    }
+    return visitedList;
+}
+
+int main()
+{
+
+    graph G;
+    node center = G.new_node();
+    queue<node> Q;
+    int x;
+    node one = G.new_node();
+    node two = G.new_node();
+    node three = G.new_node();
+    node four = G.new_node();
+    //node five = G.new_node();
+    G.new_edge(one, four);
+    G.new_edge(one, three);
+    G.new_edge(two, four);
+    //G.new_edge(three, five);
+    G.new_edge(center, one);
+    G.new_edge(center, two);
+    edge e;
+    //node_array<int> Colors(G, 0);
+    //node_array<bool> visited(G, false);
+    node_array<edge> pred(G, NULL);
+    //int levelCounter = 0;
+    node_array<int> dist(G, 0);
+    list<node> ex = my_BFS(G,center,dist,pred);
+    std::cout << ex;
     return 0;
 }
