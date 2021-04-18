b57593cf921aef7c3c37cdd6f14839ea1a90540c
George Al-Baboul <58749624+sphantom99@users.noreply.github.com>
2021-04-18 21:32:18 +0300

    Add files via upload

--- ledatest.cpp
+++ ledatest.cpp
@@ -35,7 +35,7 @@ node_array<int> my_BFS(const graph &G, node s, node_array<int> &dist, node_array
             {
                 color = 2;
             }
-           // std::cout << "different level \n";
+            // std::cout << "different level \n";
             levelCounter++;
             Q.append(NULL);
             if (Q.top() == NULL)
@@ -60,7 +60,7 @@ node_array<int> my_BFS(const graph &G, node s, node_array<int> &dist, node_array
                     visited[G.target(e)] = true;
                     pred[G.target(e)] = e;
                     //G.print_edge(e);
-                   // std::cout << "\n";
+                    // std::cout << "\n";
                 }
             }
         }
@@ -95,36 +95,72 @@ node_array<int> my_BFS(const graph &G, node s, node_array<int> &dist, node_array
 
 bool my_bipartite_checker(const graph &G, list<node> &V1, list<node> &V2)
 {
+    node s = G.first_node();
     edge e;
     //node_array<int> Colors(G, 0);
     //node_array<bool> visited(G, false);
     node_array<edge> pred(G, NULL);
     //int levelCounter = 0;
     node_array<int> dist(G, 0);
-    node_array<int> ex = my_BFS(G, 	G.first_node(), dist, pred);
+    node_array<int> ex = my_BFS(G, s, dist, pred);
+    node_array<edge> pred_from_s_to_u = pred;
+    node_array<int> dist_from_s_to_u = dist;
+    node_array<bool> visited_ancestor(G, false);
     //node_array<bool> reached(G, false);
     bool is_bipartite = true;
     forall_edges(e, G)
     {
         //std::cout << "new node \n";
-       // std::cout << ex[G.source(e)] << "\n";
-       // std::cout << ex[G.target(e)] << "\n";
-       // std::cout << "--------\n";
+        // std::cout << ex[G.source(e)] << "\n";
+        // std::cout << ex[G.target(e)] << "\n";
+        // std::cout << "--------\n";
         if (ex[G.target(e)] == ex[G.source(e)])
         {
             is_bipartite = false;
-            std::cout << "found an edge with same color on its ends\n";
-            //list<node> sth = DFS(G, center, reached);
-            //std::cout << sth << "\n";
-            node a = G.target(e);
-            while (a!= G.first_node()){
-                G.print_node(G.target(pred[a]));
-                std::cout << "------>";
-                G.print_node(G.source(pred[a]));
-                //G.print_edge(pred[a]);
-                a = G.source(pred[a]);
+            std::cout << "Starting\n";
+            node temp = G.target(e);//target;
+            while (temp != s)
+            {
+                visited_ancestor[temp] = true;
+                temp = G.source(pred[temp]);
             }
-            
+            std::cout << "Done";
+            std::cout << "Marked preds for u";
+            node source = G.source(e);
+            node lca;
+            temp = source;
+            while (temp != s)
+            {
+                if (visited_ancestor[temp] = false)
+                {
+                    temp = G.source(pred[temp]);
+                }
+                else
+                {
+                    lca = G.source(pred[temp]);
+                    break;
+                }
+            }
+            std::cout << "Marked for v";
+            temp = G.target(e);
+            G.print_node(G.target(e));
+            std::cout << "------>";
+            G.print_node(G.source(e));
+            while (temp != lca)
+            {
+                G.print_node(G.target(pred[temp]));
+                std::cout << "------>";
+                G.print_node(G.source(pred[temp]));
+                temp = G.source(pred[temp]);
+            }
+            temp = source;
+            while (temp != lca)
+            {
+                G.print_node(G.target(pred[temp]));
+                std::cout << "------>";
+                G.print_node(G.source(pred[temp]));
+                temp = G.source(pred[temp]);
+            }
         }
     }
     return is_bipartite;
@@ -139,36 +175,45 @@ int main()
     node one = G.new_node();
     node two = G.new_node();
     node three = G.new_node();
+    node four = G.new_node();
+    node five = G.new_node();
+
     //node four = G.new_node();
     //node five = G.new_node();
-    G.new_edge(center, three);
-    G.new_edge(center, two);
-    G.new_edge(two, one);
     G.new_edge(center, one);
-    G.new_edge(three, center);
-    G.new_edge(two, center);
     G.new_edge(one, two);
+    G.new_edge(one, three);
+    //G.new_edge(two, three);
     G.new_edge(one, center);
+    G.new_edge(two, one);
+    G.new_edge(three, one);
+    //G.new_edge(three, two);
+    G.new_edge(two,four);
+    G.new_edge(four,two);
+    G.new_edge(three,five);
+    G.new_edge(five,three);
+    G.new_edge(four,five);
+    G.new_edge(five,four);
     list<node> A;
     list<node> B;
     bool a = Is_Bipartite(G, A, B);
     std::cout << a << "\n";
-    for (int i = 0; i < A.size(); i++)
-    {
-        node a = A[i];
-       std::cout << G.print_node(a);
-    }
     
+    // for (int i = 0; i < A.size(); i++)
+    // {
+    //     node a = A[i];
+    //     std::cout << G.print_node(a);
+    // }
+
     // //std::cout << B << "\n";
     // forall_nodes(one, G)
     // {
     //     std::cout << one << "\n";
     // }
-    bool isBipartite = my_bipartite_checker(G,A,B);
+    bool isBipartite = my_bipartite_checker(G, A, B);
     std::cout << isBipartite;
     // G.new_edge(center, one);
     // G.new_edge(center, two);
-
 
     return 0;
 }
